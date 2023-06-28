9bbec0f8d22e188bdf8a811ab212aeec0a7452e9
shadowplay1 <shadowowplay483@gmail.com>
2023-06-28 21:44:25 +0300

    docs: sources update for v1.0.0

--- src/src/Giveaways.ts
+++ src/src/Giveaways.ts
@@ -658,3 +658,403 @@ export class Giveaways<TDatabaseType extends DatabaseType> extends Emitter<IGive
         }
     }
 }
+
+
+
+// For documentation purposes
+
+/**
+ * An object that contains an information about a giveaway.
+ * @typedef {object} IGiveaway
+ * @prop {number} id The ID of the giveaway.
+ * @prop {string} prize The prize of the giveaway.
+ * @prop {string} time The time of the giveaway.
+ * @prop {GiveawayState} state The state of the giveaway.
+ * @prop {number} winnersCount The number of possible winners in the giveaway.
+ * @prop {number} startTimestamp The timestamp when the giveaway started.
+ * @prop {boolean} isEnded Determines if the giveaway was ended in the database.
+ * @prop {number} endTimestamp The timestamp when the giveaway ended.
+ * @prop {string} hostMemberID The ID of the host member.
+ * @prop {string} channelID The ID of the channel where the giveaway is held.
+ * @prop {string} messageID The ID of the giveaway message.
+ * @prop {string} messageURL The URL of the giveaway message.
+ * @prop {string} guildID The ID of the guild where the giveaway is held.
+ * @prop {number} entries The number of giveaway entries.
+ * @prop {string[]} entriesArray The array of user IDs of users that have entered the giveaway.
+ * @prop {IGiveawayMessageProps} messageProps The message data properties for embeds and buttons.
+ *
+ * @template TDatabaseType The database type that will be used in the module.
+ */
+
+/**
+ * An interface containing embed objects for various giveaway reroll cases.
+ * @typedef {object} IGiveawayRerollEmbeds
+ * @prop {IGiveawayEmbedOptions} onlyHostCanReroll The options for the embed when only the host can reroll.
+ * @prop {IGiveawayEmbedOptions} newGiveawayMessage The options for the embed when sending a new giveaway message.
+ * @prop {IGiveawayEmbedOptions} successMessage The options for the embed when the giveaway is successful.
+ */
+
+/**
+ * An interface containing embed objects for various giveaway finish cases.
+ * @typedef {object} IGiveawayFinishEmbeds
+ * @prop {IGiveawayEmbedOptions} newGiveawayMessage The options for the embed when sending a new giveaway message.
+ * @prop {IGiveawayEmbedOptions} endMessage The options for the embed when the giveaway has ended.
+ * @prop {IGiveawayEmbedOptions} noWinners The options for the embed when there are no winners for the giveaway.
+ *
+ * @prop {IGiveawayEmbedOptions} noWinnersEndMessage
+ * The options for the embed when there are no winners for the giveaway and it has ended.
+ */
+
+/**
+ * An interface that contains the data properties for embeds and buttons.
+ * @typedef {object} IGiveawayMessageProps
+ * @prop {IGiveawayEmbeds} embeds The embed objects for the giveaway message.
+ * @prop {IGiveawayButtons} buttons The button objects for the giveaway message.
+ */
+
+/**
+ * An interface containing different types of giveaway embeds in the IGiveaways class.
+ * @typedef {object} IGiveawayEmbeds
+ * @prop {IGiveawayEmbedOptions} start Message embed data for cases when the giveaway has started.
+ * @prop {IGiveawayRerollEmbeds} reroll Message embed data for cases when rerolling the giveaway.
+ * @prop {IGiveawayFinishEmbeds} finish Message embed data for cases when the giveaway has finished.
+ */
+
+/**
+ * An object that contains the giveaway buttons that may be set up.
+ * @typedef {object} IGiveawayMessageButtons
+ * @prop {IGiveawayButtonOptions} joinGiveawayButton The options for the join giveaway button.
+ * @prop {IGiveawayButtonOptions} rerollButton The options for the reroll button.
+ * @prop {IGiveawayButtonOptions} goToMessageButton The options for the go to message button.
+ */
+
+/**
+ * An object that contains an information about a giveaway without internal props.
+ * @typedef {object} GiveawayWithoutInternalProps
+ * @prop {number} id The ID of the giveaway.
+ * @prop {string} prize The prize of the giveaway.
+ * @prop {string} time The time of the giveaway.
+ * @prop {number} winnersCount The number of possible winners in the giveaway.
+ * @prop {number} startTimestamp The timestamp when the giveaway started.
+ * @prop {number} endTimestamp The timestamp when the giveaway ended.
+ * @prop {string} hostMemberID The ID of the host member.
+ * @prop {string} channelID The ID of the channel where the giveaway is held.
+ * @prop {string} messageID The ID of the giveaway message.
+ * @prop {string} messageURL The URL of the giveaway message.
+ * @prop {string} guildID The ID of the guild where the giveaway is held.
+ * @prop {string[]} entriesArray The array of user IDs of users that have entered the giveaway.
+ * @prop {IGiveawayMessageProps} messageProps The message data properties for embeds and buttons.
+ */
+
+/**
+ * An enum that determines the state of a giveaway.
+ * @typedef {number} GiveawayState
+ * @prop {number} STARTED The giveaway has started.
+ * @prop {number} ENDED The giveaway has ended.
+ */
+
+
+
+/**
+ * @typedef {object} IGiveawaysConfiguration
+ * Full @see Giveaways class configuration object.
+ * @prop {DatabaseType} database Database type to use.
+ * @prop {DatabaseConnectionOptions} connection Database type to use.
+ *
+ * @prop {?number} [giveawaysCheckingInterval=1000]
+ * Determines how often the giveaways ending state will be checked (in ms). Default: 1000.
+ *
+ * @prop {?boolean} [debug=false] Determines if debug mode is enabled. Default: false.
+ * @prop {?number} [minGiveawayEntries=1] Determines the minimum required giveaway entries to draw the winner. Default: 1
+ * @prop {Partial} [updatesChecker] Updates checker configuration.
+ * @prop {Partial} [configurationChecker] Giveaways config checker configuration.
+ *
+ * @template {DatabaseType} TDatabaseType
+ * The database type that will determine which connection configuration should be used.
+ */
+
+/**
+ * Optional configuration for the @see Giveaways class.
+ * @typedef {object} IGiveawaysOptionalConfiguration
+ *
+ * @prop {?number} [giveawaysCheckingInterval=1000]
+ * Determines how often the giveaways ending state will be checked (in ms). Default: 1000.
+ *
+ * @prop {?boolean} [debug=false] Determines if debug mode is enabled. Default: false.
+ * @prop {?number} [minGiveawayEntries=1] Determines the minimum required giveaway entries to draw the winner. Default: 1
+ * @prop {Partial} [updatesChecker] Updates checker configuration.
+ * @prop {Partial} [configurationChecker] Giveaways config checker configuration.
+ */
+
+/**
+ * Configuration for the updates checker.
+ * @typedef {object} IUpdateCheckerConfiguration
+ * @prop {?boolean} [checkUpdates=true] Sends the update state message in console on start. Default: true.
+ * @prop {?boolean} [upToDateMessage=false] Sends the message in console on start if module is up to date. Default: false.
+ */
+
+/**
+ * Configuration for the configuration checker.
+ * @typedef {object} IGiveawaysConfigCheckerConfiguration
+ * @prop {?boolean} ignoreInvalidTypes Allows the method to ignore the options with invalid types. Default: false.
+ * @prop {?boolean} ignoreUnspecifiedOptions Allows the method to ignore the unspecified options. Default: true.
+ * @prop {?boolean} ignoreInvalidOptions Allows the method to ignore the unexisting options. Default: false.
+ * @prop {?boolean} showProblems Allows the method to show all the problems in the console. Default: true.
+ * @prop {?boolean} sendLog Allows the method to send the result in the console.
+ * Requires the 'showProblems' or 'sendLog' options to set. Default: true.
+ * @prop {?boolean} sendSuccessLog Allows the method to send the result if no problems were found. Default: false.
+ */
+
+/**
+ * JSON database configuration.
+ * @typedef {object} IJSONDatabaseConfiguration
+ * @prop {?string} [path='./giveaways.json'] Full path to a JSON storage file. Default: './giveaways.json'.
+ * @prop {?boolean} [checkDatabase=true] Checks the if there are errors in database file. Default: true.
+ * @prop {?number} [checkingCountdown=1000] Determines how often the database file will be checked (in ms). Default: 1000.
+ */
+
+/**
+ * An object that contains an information about a giveaway that is required fo starting..
+ * @typedef {object} IGiveawayData
+ * @prop {string} prize The prize of the giveaway.
+ * @prop {string} time The time of the giveaway.
+ * @prop {number} winnersCount The number of possible winners in the giveaway.
+ * @prop {string} hostMemberID The ID of the host member.
+ * @prop {string} channelID The ID of the channel where the giveaway is held.
+ * @prop {string} guildID The ID of the guild where the giveaway is held.
+ */
+
+/**
+ * Giveaway start config.
+ * @typedef {object} IGiveawayStartConfig
+ * @prop {string} prize The prize of the giveaway.
+ * @prop {string} time The time of the giveaway.
+ * @prop {number} winnersCount The number of possible winners in the giveaway.
+ * @prop {string} hostMemberID The ID of the host member.
+ * @prop {string} channelID The ID of the channel where the giveaway is held.
+ * @prop {string} guildID The ID of the guild where the giveaway is held.
+ * @prop {IGiveawayButtons} [buttons] Giveaway buttons object.
+ * @prop {IGiveawayButtons} [defineEmbedStrings] Giveaway buttons object.
+ */
+
+/**
+ * Giveaway buttons that may be specified.
+ * @typedef {object} IGiveawayButtons
+ * @prop {?IGiveawayButtonOptions} [joinGiveawayButton] Button object for the "join giveaway" button.
+ * @prop {?IGiveawayButtonOptions} [rerollButton] Button object for the "reroll" button.
+ * @prop {?ILinkButton} [goToMessageButton] Link button object for the "go to message" button.
+ */
+
+/**
+ * Link button object.
+ *
+ * Please note that URL is not required as it's being applied after starting the giveaway.
+ * @typedef {object} ILinkButton
+ * @prop {string} [text] Button text string.
+ * @prop {string} [emoji] Emoji string.
+ * @prop {ButtonStyle} url URL that the button will take to.
+ */
+
+/**
+ * A function that defines the embed strings used in the giveaway.
+ * @callback DefineEmbedStringsCallback
+ * @param {Omit} giveaway - An object containing information about the giveaway.
+ * @param {User} giveawayHost - The host of the giveaway.
+ * @returns {Partial} - An object containing the defined embed strings.
+ */
+
+/**
+ * Giveaway start options.
+ * @typedef {object} IGiveawayStartOptions
+ * @prop {IGiveawayButtons} [buttons] Giveaway buttons object.
+ * @prop {IGiveawayButtons} [defineEmbedStrings] Giveaway buttons object.
+ */
+
+/**
+ * An object that contains messages that are sent in various giveaway cases, such as end with winners or without winners.
+ * @typedef {object} IGiveawayMessages
+ * @prop {IGiveawayEmbedOptions} newGiveawayMessage Original giveaway message will be edited with this message object.
+ * @prop {IGiveawayEmbedOptions} endMessage The message sent in the giveaway channel when a giveaway ends with winners.
+ * @prop {IGiveawayEmbedOptions} noWinners The message sent in the giveaway channel when a giveaway ends without winners.
+ *
+ * @prop {IGiveawayEmbedOptions} noWinnersEndMessage
+ * The message sent in the giveaway channel when a giveaway ends without winners and there are no more participants.
+ */
+
+/**
+ * A function that is called when giveaway is finished.
+ * @callback GiveawayFinishCallback
+ * @param {string} winnersString A string that contains the users that won the giveaway separated with comma.
+ * @param {number} numberOfWinners Number of winners that were picked.
+ * @returns {IGiveawayMessages} Giveaway message objects.
+ */
+
+/**
+ * An object that contains messages that are sent in various giveaway cases, such as end with winners or without winners.
+ * @typedef {object} IGiveawayMessages
+ * @prop {IGiveawayEmbedOptions} onlyHostCanReroll
+ * The message sent in the giveaway channel when not a giveaway host tries to do a reroll.
+ *
+ * @prop {IGiveawayEmbedOptions} newGiveawayMessage Original giveaway message will be edited with this message object.
+ *
+ * @prop {IGiveawayEmbedOptions} successMessage
+ * The message sent in the giveaway channel when a giveaway ends without winners.
+ */
+
+/**
+ * A function that is called when giveaway winners are rerolled.
+ * @callback GiveawayRerollCallback
+ * @param {string} winnersString A string that contains the users that won the giveaway separated with comma.
+ * @param {number} numberOfWinners Number of winners that were picked.
+ * @returns {IGiveawayMessages} Giveaway message objects.
+ */
+
+/**
+ * Object containing embed string definitions used in the IGiveaways class.
+ * @typedef {object} IEmbedStringsDefinitions
+ *
+ * @prop {IGiveawayEmbedOptions} start
+ * This object is used in the original giveaway message that people will use to join the giveaway.
+ *
+ * @prop {GiveawayFinishCallback} finish
+ * This function is called and all returned message objects are extracted and used when the giveaway is finished.
+ *
+ * @prop {GiveawayRerollCallback} reroll
+ * This function is called and all returned message objects are extracted and used when the giveaway winners are rerolled.
+ */
+
+/**
+ * Button object.
+ * @typedef {object} IGiveawayButtonOptions
+ * @prop {?string} [text] Button text string.
+ * @prop {?string} [emoji] Emoji string.
+ * @prop {?ButtonStyle} [style] Button style.
+ */
+
+/**
+ * Message embed options.
+ * @typedef {object} IGiveawayEmbedOptions
+ * @prop {?string} [messageContent] The content of the message. If only this is specified
+ * @prop {?string} [title] The title of the embed.
+ * @prop {?string} [titleIcon] The icon of the title in the embed.
+ * @prop {?string} [titleIconURL] The url of the icon of the title in the embed.
+ * @prop {?string} [description] The description of the embed.
+ * @prop {?string} [footer] The footer of the embed.
+ * @prop {?string} [footerIcon] The icon of the footer in the embed.
+ * @prop {?string} [thumbnailURL] Embed thumbnail.
+ * @prop {?string} [imageURL] Embed Image URL.
+ * @prop {ColorResolvable} [color] The color of the embed.
+ */
+
+/**
+ * Database connection options based on the used database type.
+ *
+ * @see Partial<IJSONDatabseConfiguration> - JSON configuration.
+ *
+ * @see EnmapOptions<any, any> - Enmap configuration.
+ *
+ * @see IMongoConnectionOptions - MongoDB connection configuration.
+ *
+ * @typedef {(
+ * Partial<IJSONDatabseConfiguration> | EnmapOptions<any, any> | IMongoConnectionOptions
+ * )} DatabaseConnectionOptions<TDatabaseType>
+ *
+ * @template {DatabaseType} TDatabaseType
+ * The database type that will determine which connection configuration should be used.
+ */
+
+/**
+ * @see null - JSON database management object - `null`
+ * is because it's not an external database - JSON is being parsed by the module.
+ *
+ * @see Enmap<string, IDatabaseStructure> - Enmap database.
+ *
+ * @see Mongo<IDatabaseStructure> - MongoDB database.
+ *
+ * @typedef {(
+ * null | Enmap<string, IDatabaseStructure> | Mongo<IDatabaseStructure>
+ * )} Database<TDatabaseType> External database object based on the used database type.
+ *
+ * @template {DatabaseType} TDatabaseType
+ * The database type that will determine which external database management object should be used.
+ */
+
+
+/**
+ * An interface containing the structure of the database used in the IGiveaways class.
+ * @typedef {object} IDatabaseStructure
+ * @prop {any} guildID Guild ID that stores the giveaways array
+ * @prop {IGiveaway[]} giveaways Giveaways array property inside the [guildID] object in database.
+ */
+
+/**
+ * The giveaway data that stored in database,
+ * @typedef {object} IDatabaseGiveaway
+ * @prop {IGiveaway} giveaway Giveaway object.
+ * @prop {number} giveawayIndex Giveaway index in the guild giveaways array.
+ */
+
+
+/**
+ * A type containing all the @see Giveaways events and their return types..
+ * @typedef {object} IGiveawaysEvents
+ * @prop {Giveaways<DatabaseType>} ready Emits when the @see Giveaways is ready.
+ * @prop {void} databaseConnect Emits when the connection to the database is established.
+ * @prop {Giveaway<DatabaseType>} giveawayStart Emits when a giveaway is started.
+ * @prop {Giveaway<DatabaseType>} giveawayRestart Emits when a giveaway is rerolled.
+ * @prop {Giveaway<DatabaseType>} giveawayEnd Emits when a giveaway is rerolled.
+ * @prop {IGiveawayRerollEvent} giveawayReroll Emits when a giveaway is rerolled.
+ *
+ * @template {DatabaseType} TDatabaseType The database type that will be used in the module.
+ */
+
+/**
+ * Giveaway reroll event object.
+ * @typedef {object} IGiveawayRerollEvent
+ * @prop {Giveaway<DatabaseType>} giveaway Giveaway instance.
+ * @prop {string} newWinners Array of the new picked winners after reroll.
+ *
+ * @template {DatabaseType} TDatabaseType The database type that will be used in the module.
+ */
+
+
+
+
+// Events, for documentation purposes
+
+/**
+ * Emits when the @see Giveaways module is ready.
+ * @event Giveaways#ready
+ * @param {Giveaways<DatabaseType>} giveaways Initialized @see Giveaways instance.
+ */
+
+/**
+ * Emits when the @see Giveaways module establishes the database connection.
+ * @event Giveaways#databaseConnect
+ * @param {void} databaseConnect Initialized @see Giveaways instance.
+ */
+
+/**
+ * Emits when a giveaway is started.
+ * @event Giveaways#giveawayStart
+ * @param {Giveaway<DatabaseType>} giveaway @see Giveaway that started.
+ */
+
+/**
+ * Emits when a giveaway is restarted.
+ * @event Giveaways#giveawayRestart
+ * @param {Giveaway<DatabaseType>} giveaway @see Giveaway that restarted.
+ */
+
+/**
+ * Emits when a giveaway is ended.
+ * @event Giveaways#giveawayEnd
+ * @param {Giveaway<DatabaseType>} giveaway @see Giveaway that ended.
+ */
+
+/**
+ * Emits when a giveaway is rerolled.
+ * @event Giveaways#giveawayReroll
+ * @param {IGiveawayRerollEvent} giveaway @see Giveaway that was rerolled.
+ */
+
