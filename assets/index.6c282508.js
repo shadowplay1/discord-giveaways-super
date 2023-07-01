a5b785447c662b20008d7e73dd7cbc300502dda3
shadowplay1 <shadowplay483@gmail.com>
2023-07-01 23:15:24 +0300

    .

--- assets/index.6c282508.js
+++ assets/index.6c282508.js
@@ -1,1 +1,1 @@
-import{_ as e}from"./Spinner.ee5f11f6.js";import{d as o,o as r,b as s}from"./assets/vendor.5101602c.js";var a=o({expose:[],setup:o=>(o,a)=>(r(),s(e))});export default a;
+import{_ as e}from"./Spinner.ee5f11f6.js";import{d as o,o as r,b as s}from"./vendor.5101602c.js";var a=o({expose:[],setup:o=>(o,a)=>(r(),s(e))});export default a;
